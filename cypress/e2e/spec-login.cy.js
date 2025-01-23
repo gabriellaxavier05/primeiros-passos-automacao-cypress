b9668bd67ac1bb05dc58054b88549f39ba0dea5d
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-01-23 17:53:08 -0300

    Finalizando cenário de login com sucesso

--- cypress/e2e/spec-login.cy.js
+++ cypress/e2e/spec-login.cy.js
@@ -4,5 +4,8 @@ describe('Orange HRM Testes', () => {
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin') // Preenche o campo 'Username' com o valor 'Admin'
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123') // Preenche o campo 'Password' com o valor 'admin123'
     cy.get('.oxd-button').click() // Clica no botão 'Login'
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index') // Verifica se a URL é igual ao que foi passado
+    // Outra forma de verificar a página correta
+    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard') // Verifica se o texto 'Dashboard' está no título da página
   })
 })
