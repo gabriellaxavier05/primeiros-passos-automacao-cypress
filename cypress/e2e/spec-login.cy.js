415c30608d0a083d42aa49140e1225a4291553af
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-01-31 19:08:43 -0300

    Usando CSS Selector e alterando atributos da automação

--- cypress/e2e/spec-login.cy.js
+++ cypress/e2e/spec-login.cy.js
@@ -1,21 +1,21 @@
 describe('Orange HRM Testes', () => {
   // Teste 1: Login com sucesso
-  it.skip('Login com sucesso', () => { // Nome do teste
+  it('Login com sucesso', () => { // Nome do teste
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') // Passando a URL
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin') // Preenche o campo 'Username' com o valor 'Admin'
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123') // Preenche o campo 'Password' com o valor 'admin123'
-    cy.get('.oxd-button').click() // Clica no botão 'Login'
+    cy.get("[name='username']").type('Admin') // Preenche o campo 'Username' com o valor 'Admin'
+    cy.get("[name='password']").type('admin123') // Preenche o campo 'Password' com o valor 'admin123'
+    cy.get("[type='submit']").click() // Clica no botão 'Login'
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') // Verifica se a URL é igual ao que foi passado
     // Outra forma de verificar a página correta
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard') // Verifica se o texto 'Dashboard' está no título da página
+    cy.get(".oxd-topbar-header-breadcrumb-module").contains('Dashboard') // Verifica se o texto 'Dashboard' está no título da página
   })
 
   // Teste 2: Login com falha
   it('Login com falha', () => { // Nome do teste
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') // Passando a URL
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Teste') // Preenche o campo 'Username' com o usuário inválido
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('teste') // Preenche o campo 'Password' com a senha inválida
-    cy.get('.oxd-button').click() // Clica no botão 'Login'
-    cy.get('.oxd-alert') // Verifica se o alerta de erro apareceu  
+    cy.get("[name='username']").type('Teste') // Preenche o campo 'Username' com o usuário inválido
+    cy.get("[name='password']").type('teste') // Preenche o campo 'Password' com a senha inválida
+    cy.get("[type='submit']").click() // Clica no botão 'Login'
+    cy.get("[role='alert']") // Verifica se o alerta de erro apareceu  
   })
 })
