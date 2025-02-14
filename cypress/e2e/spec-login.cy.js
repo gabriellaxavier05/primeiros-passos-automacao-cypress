8da82c74229b9288c9520015ba02c93a3b7746e8
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-02-14 03:04:01 -0300

    Usando dados de testes em objetos

--- cypress/e2e/spec-login.cy.js
+++ cypress/e2e/spec-login.cy.js
@@ -1,6 +1,6 @@
 describe('Orange HRM Testes', () => {
 
-  // Objetos de seletores criados para serem usados nos testes
+  // Objetos de seletores criados para serem usados nos testes:
   const listaSeletores = {
     usernameField: "[name='username']", // campo de usuário
     passwordField: "[name='password']", // campo de senha
@@ -10,11 +10,24 @@ describe('Orange HRM Testes', () => {
     credencialErradaAlerta: "[role='alert']" // Alerta de credenciais inválidas
   }
 
+  // Dados para serem usados nos objetos:
+  const dadosUsuario = {
+    usuarioSucesso: {
+      username: 'Admin',
+      password: 'admin123'
+    },
+    usuarioFalha: {
+      username: 'Teste',
+      password: 'teste'
+    }
+  }
+  
+
   // Teste 1: Login com sucesso
   it('Login com sucesso', () => { // Nome do teste
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') // Passando a URL
-    cy.get(listaSeletores.usernameField).type('Admin') // Preenche o campo de usuário com o valor 'Admin'
-    cy.get(listaSeletores.passwordField).type('admin123') // Preenche o campo de senha com o valor 'admin123'
+    cy.get(listaSeletores.usernameField).type(dadosUsuario.usuarioSucesso.username) // Preenche o campo de usuário com o valor 'Admin' usando os dados da const 'dadosUsuario' ('usuarioSucesso'/'username')
+    cy.get(listaSeletores.passwordField).type(dadosUsuario.usuarioSucesso.password) // Preenche o campo de senha usando os dados da const 'dadosUsuario' ('usuarioSucesso'/'password')
     cy.get(listaSeletores.loginButton).click() // Clica no botão 'Login'
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') // Verifica se a URL é igual ao que foi passado
     // Outra forma de verificar a página correta
@@ -24,8 +37,8 @@ describe('Orange HRM Testes', () => {
   // Teste 2: Login com falha
   it('Login com falha', () => { // Nome do teste
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') // Passando a URL
-    cy.get(listaSeletores.usernameField).type('Teste') // Preenche o campo de usuário com o usuário inválido
-    cy.get(listaSeletores.passwordField).type('teste') // Preenche o campo de senha com a senha inválida
+    cy.get(listaSeletores.usernameField).type(dadosUsuario.usuarioFalha.username) // Preenche o campo de usuário com o usuário inválido usando os dados da const 'dadosUsuario' ('usuarioFalha'/'username')
+    cy.get(listaSeletores.passwordField).type(dadosUsuario.usuarioFalha.password) // Preenche o campo de senha com a senha inválida usando os dados da const 'dadosUsuario' ('usuarioFalha'/'password')
     cy.get(listaSeletores.loginButton).click() // Clica no botão 'Login'
     cy.get(listaSeletores.credencialErradaAlerta) // Verifica se o alerta de erro apareceu  
   })
