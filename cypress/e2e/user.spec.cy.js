0a28632d671bede5783fe435d9e491d3414898fd
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-05-08 17:47:38 -0300

    Subindo a 1a parte da alteração da feature MyInfo com Page Objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -3,20 +3,18 @@ import userData from '../fixtures/userData.json' // Importando o arquivo JSON qu
 import LoginPage from '../../pages/loginPage.js' // Importando a classe LoginPage
 import DashboardPage from '../../pages/dashboardPage.js' // Importando a classe DashboardPage
 import MenuPage from '../../pages/menuPage.js' // Importando a classe MenuPage
+import MyInfoPage from '../../pages/myInfoPage.js' // Importando a classe MyInfoPage
 
 
 // INSTANCIAÇÕES
 const loginPage = new LoginPage() // Instanciando a classe LoginPage
 const dashboardPage = new DashboardPage() // Instanciando a classe DashboardPage
 const menuPage = new MenuPage() // Instanciando a classe MenuPage
+const myInfoPage = new MyInfoPage() // Instanciando a classe MyInfoPage
 
 describe('Orange HRM Testes', () => {
   // Objetos de seletores criados para serem usados nos testes:
   const listaSeletores = {
-    firstNameField: "[name='firstName']", // Campo de nome
-    lastNameField: "[name='lastName']", // Campo de sobrenome
-    genericField: ".oxd-input--active", // Campo genérico para ser usado em outros testes
-    dateField: "[placeholder='mm-dd-yyyy']", // Campo de data
     dateCloseButton: ".--close", // Botão de fechar o calendário
     genericCombobox: ".oxd-select-text--arrow",
     opcaoComboboxNationallity: ".oxd-select-dropdown > :nth-child(5)", // Opção de nacionalidade "American"
@@ -33,28 +31,19 @@ describe('Orange HRM Testes', () => {
   })
 
   // Teste 2: Login com falha
-  it.only('Login com falha', () => { // Nome do teste
+  it('Login com falha', () => { // Nome do teste
     loginPage.acessoPaginaLogin() // Acessa a página de login
     loginPage.loginComUsuario(userData.usuarioFalha.username, userData.usuarioFalha.password) // Faz o login com usuário e senha inválidos
   })
 
   // Novo cenário de teste para verificar a atualização de informações do usuário feita com sucesso
-  it('Atualização de informações do usuário com sucesso', () => { // it.only() é usado para rodar apenas este teste
-    cy.visit('/auth/login') // Passando a URL; URL básica definida no arquivo cypress.config.js
-    cy.get(listaSeletores.usernameField).type(userData.usuarioSucesso.username) // Preenche o campo de usuário com o valor 'Admin' usando os dados de 'usuarioSucesso'/'username' do arquivo userData.json
-    cy.get(listaSeletores.passwordField).type(userData.usuarioSucesso.password) // Preenche o campo de senha usando os dados de 'usuarioSucesso'/'password' do arquivo userData.json
-    cy.get(listaSeletores.loginButton).click() // Clica no botão 'Login'
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index') // Verifica se a URL é igual ao que foi passado
-    // Outra forma de verificar a página correta
-    cy.get(listaSeletores.dashboardGrid) // Verifica se o grid do dashboard está presente
-    cy.get(listaSeletores.myInfoButton).click() // Clica no link 'My Info'
-    cy.get(listaSeletores.firstNameField).clear().type('Nome Teste') // Limpa o campo de nome e preenche com o valor 'Nome Teste'
-    cy.get(listaSeletores.lastNameField).clear().type('Sobrenome Teste') // Limpa o campo de sobrenome e preenche com o valor 'Sobrenome Teste'
-    //cy.get(listaSeletores.genericField).eq(3).clear().type('NickName Teste') // Preenche o campo "Nick Name" com o valor 'NickName Teste'
-    cy.get(listaSeletores.genericField).eq(3).clear().type('EmployeeT') // Preenche o campo "Employee Id" com o valor 'EmployeeT'
-    cy.get(listaSeletores.genericField).eq(5).clear().type('13579') // Preenche o campo "Other Id" com o valor '135794
-    cy.get(listaSeletores.genericField).eq(5).clear().type('123456789') // Preenche o campo "Driver's License Number" com o valor '123456789'
-    cy.get(listaSeletores.genericField).eq(6).clear().type('2025-04-29') // Preenche o campo "License Expiry Date" com o valor '2025-04-29'
+  it.only('Atualização de informações do usuário com sucesso', () => { // it.only() é usado para rodar apenas este teste
+    loginPage.acessoPaginaLogin() // Acessa a página de login
+    loginPage.loginComUsuario(userData.usuarioSucesso.username, userData.usuarioSucesso.password) // Faz o login com usuário e senha válidos
+    dashboardPage.checkDashboardPage() // Verifica se está na página do dashboard
+    menuPage.acessoMyInfo() // Acessa a página 'My Info'
+    myInfoPage.preenchimentoPersonalDetails('Nome Teste', 'Sobrenome Teste', 'EmployeeT', '13579', '123456789', '2025-04-29') // Preenche os detalhes pessoais com informações passadas por aqui
+
     cy.get(listaSeletores.dateCloseButton).click() // Clica no botão de fechar o calendário
     cy.get(listaSeletores.genericCombobox).eq(0).click() // Clica no combobox "Nationality"
     cy.get(listaSeletores.opcaoComboboxNationallity).click() // Seleciona a nacionalidade "American" 
---
+++ pages/myInfoPage.js
@@ -0,0 +1,32 @@
+class MyInfoPage {
+    listaSeletores() {
+        const seletores = {
+            firstNameField: "[name='firstName']", // Campo de nome
+            lastNameField: "[name='lastName']", // Campo de sobrenome
+            genericField: ".oxd-input--active", // Campo genérico para ser usado em outros testes
+            dateField: "[placeholder='mm-dd-yyyy']", // Campo de data
+            dateCloseButton: ".--close", // Botão de fechar o calendário
+            genericCombobox: ".oxd-select-text--arrow",
+            opcaoComboboxNationallity: ".oxd-select-dropdown > :nth-child(5)", // Opção de nacionalidade "American"
+            opcaoComboboxMaritalStatus: ".oxd-select-dropdown > :nth-child(2)", // Opção de estado civil "Single"
+            submitButton: "[type='submit']", // Botão de salvar
+        }
+
+        return seletores; // Retorna o objeto de seletores
+    }
+
+    // Criação dos passos do teste
+    preenchimentoPersonalDetails(firstName, lastName, employeeId, otherId, driversLicenseNumber, licenseExpiryDate) { // Função para preencher os detalhes pessoais
+        cy.get(this.listaSeletores().firstNameField).clear().type(firstName) // Limpa o campo de nome e preenche com o 1o nome
+        cy.get(this.listaSeletores().lastNameField).clear().type(lastName) // Limpa o campo de sobrenome e preenche com o sobrenome
+        // cy.get(this.listaSeletores().genericField).eq(3).clear().type(nickName) // Preenche o campo "Nick Name"
+        cy.get(this.listaSeletores().genericField).eq(3).clear().type(employeeId) // Preenche o campo "Employee Id"
+        cy.get(this.listaSeletores().genericField).eq(5).clear().type(otherId) // Preenche o campo "Other Id"
+        cy.get(this.listaSeletores().genericField).eq(5).clear().type(driversLicenseNumber) // Preenche o campo "Driver's License Number"
+        cy.get(this.listaSeletores().genericField).eq(6).clear().type(licenseExpiryDate) // Preenche o campo "License Expiry Date"
+
+
+    }
+}
+
+export default MyInfoPage; // Exporta a classe MyInfoPage para ser usada em outros arquivos
