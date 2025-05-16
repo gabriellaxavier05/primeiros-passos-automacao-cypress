3a514dd0f301f8251569e07db55ab14493a7287f
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2025-05-15 21:22:51 -0300

    Usando ChanceJS no CT de atualizar infos do usuário - parte 2

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -5,9 +5,10 @@ import DashboardPage from '../pages/dashboardPage.js' // Importando a classe Das
 import MenuPage from '../pages/menuPage.js' // Importando a classe MenuPage
 import MyInfoPage from '../pages/myInfoPage.js' // Importando a classe MyInfoPage
 
-var Chance = require('chance');
+const Chance = require('chance');
 
 // INSTANCIAÇÕES
+const  chance = new Chance(); // Instanciando a biblioteca Chance para gerar dados aleatórios
 const loginPage = new LoginPage() // Instanciando a classe LoginPage
 const dashboardPage = new DashboardPage() // Instanciando a classe DashboardPage
 const menuPage = new MenuPage() // Instanciando a classe MenuPage
@@ -20,7 +21,7 @@ describe('Orange HRM Testes', () => {
     loginPage.loginComUsuario(userData.usuarioSucesso.username, userData.usuarioSucesso.password) // Faz o login com usuário e senha válidos
     dashboardPage.checkDashboardPage() // Verifica se está na página do dashboard
     menuPage.acessoMyInfo() // Acessa a página 'My Info'
-    myInfoPage.preenchimentoPersonalDetails('Nome Teste', 'Sobrenome Teste', 'EmployeeT', '13579', '123456789', '2025-04-29') // Preenche os detalhes pessoais com informações passadas por aqui
+    myInfoPage.preenchimentoPersonalDetails(chance.first(), chance.last(), 'VPZPVv63', '19257', '37190562', '2026-04-10') // Preenche os detalhes pessoais com alguns dados aleatórios gerados pelo ChanceJS
     myInfoPage.saveForm() // Salva o formulário
   })
 })
